731c1dee699032ff7b2a281ee99228f980a52519
Rom Rom <romdevmod@gmail.com>
2022-11-19 22:33:40 +0800

    Initial commit

---
+++ generate.sh
@@ -0,0 +1,14 @@
+#!/bin/sh - 
+
+if [[ $1 == "" && $2 == "" ]]; then
+  echo -e "
+error: missing required parameter..
+usage: ./generate.sh [input] [output]
+";
+  exit;
+fi
+
+[[ ! -d $2 ]] && mkdir $2;
+
+protoc -I=$1 --go_out=$2 --go_opt=paths=source_relative --go-grpc_out=$2 --go-grpc_opt=paths=source_relative $1/*.proto;
+
